a8dc5a6cbab0f4c621797a791393488fa3373c0d
nthnn <nathanneisip@gmail.com>
2023-04-15 17:07:45 +0800

    Modified clean-up.bat

--- build/clean-up.bat
+++ build/clean-up.bat
@@ -1,5 +1,6 @@
 @echo off
 if exist ..\src\Batch2Exe-Wrapper-Template\bin rmdir /s ..\src\Batch2Exe-Wrapper-Template\bin
 if exist ..\src\Batch2Exe-Wrapper-Template\obj rmdir /s ..\src\Batch2Exe-Wrapper-Template\obj
+if exist ..\src\Batch2Exe-GUI\out rmdir /s ..\src\Batch2Exe-GUI\out
 echo Done cleaning up...
 pause
