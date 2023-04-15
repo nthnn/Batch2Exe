80596ae5b3260c3ae97c4336de8dce03bb20090c
nthnn <nathanneisip@gmail.com>
2023-04-15 17:36:49 +0800

    Created Batch2Exe wrapper template solution.

---
+++ build/clean-up.bat
@@ -0,0 +1,5 @@
+@echo off
+if exist ..\src\Batch2Exe-Wrapper-Template\bin rmdir /s ..\src\Batch2Exe-Wrapper-Template\bin
+if exist ..\src\Batch2Exe-Wrapper-Template\obj rmdir /s ..\src\Batch2Exe-Wrapper-Template\obj
+echo Done cleaning up...
+pause
---
+++ src/Batch2Exe-Wrapper-Template/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+public class Batch2ExeWrapperTemplate {
+    private static string Title = "Hello, world!";
+    private static string Arguments = null;
+    private static string DefaultWorkingDirectory = null;
+    private static string Script = "echo \"Hello, world\";";
+    private static string FileName = "hello.bat";
+
+    public static void Main(string[] Args) {
+        File.WriteAllText(FileName, "@echo off\r\ntitle " + Title + "\r\n" + Script);
+        Process proc = new Process();
+
+        proc.StartInfo.FileName = FileName;
+        if(Arguments != null)
+            proc.StartInfo.Arguments = Arguments;
+
+        if(DefaultWorkingDirectory != null)
+            proc.StartInfo.WorkingDirectory = DefaultWorkingDirectory;
+
+        proc.Start();
+        File.SetAttributes(FileName, File.GetAttributes(FileName) | FileAttributes.Hidden);
+
+        proc.WaitForExit();
+        File.Delete(FileName);
+    }
+}
