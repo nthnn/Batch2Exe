da9dd7f82c64855280127dcf950f311e241c5965
nthnn <nathanneisip@gmail.com>
2023-04-15 17:06:25 +0800

    Added logo to main UI.

--- src/Batch2Exe-GUI/src/xyz/nthnn/batch2exe/MainUI.java
+++ src/Batch2Exe-GUI/src/xyz/nthnn/batch2exe/MainUI.java
@@ -2,8 +2,10 @@ package xyz.nthnn.batch2exe;
 
 import com.formdev.flatlaf.FlatDarkLaf;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class MainUI implements Runnable {
     private JFrame mainFrame;
@@ -18,6 +20,12 @@ public class MainUI implements Runnable {
         this.mainFrame.setSize(new Dimension(300, 300));
         this.mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.mainFrame.setLocationRelativeTo(null);
+
+        try {
+            this.mainFrame.setIconImage(ImageIO.read(Thread.currentThread().getContextClassLoader().getResourceAsStream("res/batch2exe-logo.png")));
+        }
+        catch(IOException ex) { }
+
         this.mainFrame.show();
     }
 }
