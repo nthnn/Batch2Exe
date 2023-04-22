e31a2a49f2c2a92b872e061dd26b81421ccdb4bb
nthnn <nathanneisip@gmail.com>
2023-04-22 20:47:20 +0800

    Fixed

--- build/build.bat
+++ build/build.bat
@@ -1,7 +1,4 @@
 @echo off
-collector.bat
-launcher-build.bat
-msi-build.bat
-clean-up.bat
+collector.bat && launcher-build.bat && msi-build.bat && clean-up.bat
 echo You can now check the installer at dest/Batch2Exe.msi
 pause
